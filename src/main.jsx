ea2c6cbbde8ba8f70e392fb915af07c37b7e1af6
Saurabh Raj <saurabhraj230125@gmail.com>
2025-12-30 23:34:49 +0530

    Update package and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Root() {
+  return (
+    <React.StrictMode>
+      <App />
+      <Analytics />
+    </React.StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Root />);
